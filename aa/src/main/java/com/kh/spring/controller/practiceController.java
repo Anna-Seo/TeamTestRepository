1b7133bd1e7be8e2f6d9d8a9bd70fb67672cc816
jsj <a01099350974@gmail.com>
2025-12-17 14:23:38 +0900

    Initial commit on main

---
+++ aa/src/main/java/com/kh/spring/controller/practiceController.java
@@ -0,0 +1,10 @@
+package com.kh.spring.controller;
+
+public class practiceController {
+	
+	public void controll1() {
+		System.out.println("컨트롤러1");
+		
+	}
+
+}
---
+++ aa/src/main/java/com/kh/spring/model/vo/practiceModel.java
@@ -0,0 +1,8 @@
+package com.kh.spring.model.vo;
+
+public class practiceModel {
+	
+	private String model1;
+	private String model2;
+
+}
